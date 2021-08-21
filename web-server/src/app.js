7918aea25d1a9f27f68a285a57c96403a464648e
Christopher McGale <mcga6260@mylaurier.ca>
2021-08-21 11:38:04 -0400

    Section 8 start

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths
 const publicPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 // Config app
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicPath))
@@ -32,14 +35,40 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: "Help Page",
-        helpText: "Some helpful text"
+        helpText: "Some helpful text",
+        author: 'Chris McGale'
     })
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide an address"
+        })
+    }
+
     res.send({
         forecast: 'Sunny',
-        location: 'Toronto'
+        location: 'Toronto',
+        address: req.query.address
+    })
+})
+
+
+
+app.get('/help/*', (req, res) => {
+    res.render('notFound', {
+        title: "404",
+        errorMessage: "Help Article Not Found",
+        author: "Chris McGale"
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('notFound', {
+        title: "404",
+        errorMessage: "Page Not Found",
+        author: "Chris McGale"
     })
 })
 
