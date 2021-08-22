be801496016e03c9c100679911248aa7160456bb
Christopher McGale <mcga6260@mylaurier.ca>
2021-08-22 09:37:57 -0400

    Section 8 done. Fix help and move webserver

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,26 @@
 console.log('Client side js')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const loc = document.getElementById("location")
+const fore = document.getElementById("forecast")
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    loc.textContent = "Loading..."
+    fore.textContent = ""
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            return loc.textContent = data.error
+        }
+        loc.textContent = data.location
+        fore.textContent = data.forecast
+    })
+    })
+})
+
